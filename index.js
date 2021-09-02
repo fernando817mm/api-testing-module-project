06f6ba4cfbd56f8177bdbaedc7d042a95173bc13
Fernando Martinez <fernando817mm@gmail.com>
2021-09-02 15:05:56 -0500

    mvp endpoints done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const yup = require("yup");
 
 const server = express();
 
@@ -12,41 +13,80 @@ server.use(morgan("dev"));
 
 const PORT = process.env.PORT || 5000;
 
+const users = [
+  {
+    username: "Fernando",
+    password: "Fernando",
+    id: 1,
+  },
+  {
+    username: "Aliyah",
+    password: "Aliyah",
+    id: 2,
+  },
+  {
+    username: "Benito",
+    password: "Benito",
+    id: 3,
+  },
+];
+
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
 
 server.get("/api/users", (req, res) => {
-  res.json({
-    users: [
-      {
-        name: "Fernando",
-        email: "fernando@something.com",
-        id: 1,
-      },
-      {
-        name: "Aliyah",
-        email: "aliyah@something.com",
-        id: 2,
-      },
-      {
-        name: "Benito",
-        email: "benito@something.com",
-        id: 3,
-      },
-    ],
-  });
+  res.json(users);
 });
 
-server.get("/api/users/:id", (req, res) => {
-  const { id } = req.params;
-  res.json({
-    message: `Current id parameter is ${id}`,
-  });
+const newUserSchema = yup.object({
+  username: yup.string().required(),
+  password: yup.string().required(),
+});
+
+server.post("/api/register", (req, res, next) => {
+  const newUser = req.body;
+  newUserSchema
+    .validate(newUser)
+    .then((user) => {
+      const newUser = { ...user, id: Date.now() };
+      users.push(newUser);
+      res.json(newUser);
+    })
+    .catch(() => {
+      next({
+        message: `username and password are required`,
+        status: 402,
+      });
+    });
+});
+
+server.post("/api/login", (req, res, next) => {
+  newUserSchema
+    .validate(req.body)
+    .then((user) => {
+      console.log(user);
+      res.json({
+        message: `Welcome, ${user.username}!`,
+      });
+    })
+    .catch(() => {
+      next({
+        message: `username and password are required`,
+        status: 402,
+      });
+    });
 });
 
 server.get("*", (req, res) => {
   res.send(`
     <h1>Fernando Martinez</h1>
   `);
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
 });
